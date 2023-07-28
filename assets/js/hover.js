ad18cb3965bee1b6a932af7c8f2e9b55c672cf19
Sudipto Shahnawaz Shanto <shahnawazshanto@gmail.com>
2023-07-28 16:25:38 +0600

    Hover animation

---
+++ assets/js/hover.js
@@ -0,0 +1,20 @@
+$(function() {
+    $("#VindictiveCats").hover(
+        function() {
+            $(this).attr("src", "images/Vindictive Cats.gif");
+        },
+        function() {
+            $(this).attr("src", "images/vindictive cats - cover.png");
+        }                         
+    );   
+    
+    
+    $("#NoobCube").hover(
+        function() {
+            $(this).attr("src", "images/NoobCube.gif");
+        },
+        function() {
+            $(this).attr("src", "images/noobcube - cover.png");
+        }                         
+    );    
+});
